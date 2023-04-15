a32b861b48470c081ae4704125747fdd876b36ef
heriique <ericdecaprona@hotmail.com>
2023-04-15 13:23:56 +0200

    Added MyBluetooth

--- app/src/main/java/com/example/waterplants/MainActivity.kt
+++ app/src/main/java/com/example/waterplants/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var myBluetooth: MyBluetooth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -32,5 +34,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        myBluetooth = MyBluetooth(this)
     }
 }
